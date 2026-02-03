2574f0fabdbaef6d77f69d6d1426d5d444ffb3f7
brian lodeki <lodekib@gmail.com>
2026-02-03 10:21:36 +0300

    Added the fillables and casts to missing models

--- app/Models/BusinessLocation.php
+++ app/Models/BusinessLocation.php
@@ -9,4 +9,19 @@ class BusinessLocation extends Model
 {
     /** @use HasFactory<\Database\Factories\BusinessLocationFactory> */
     use HasFactory;
+
+
+    protected $fillable = [
+        'business_profile_id','name','address','city','county','postal_code','latitude','longitude',
+        'is_primary','is_active',
+    ];
+
+    protected $casts = [
+        'latitude' => 'decimal',
+        'longitude' => 'decimal',
+        'is_primary' => 'boolean',
+        'is_active' => 'boolean'
+    ];
+
+
 }
--- app/Models/BusinessProfile.php
+++ app/Models/BusinessProfile.php
@@ -4,9 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class BusinessProfile extends Model
 {
     /** @use HasFactory<\Database\Factories\BusinessProfileFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'user_id','business_name','business_type','description','website','instagram','facebook','whatsapp',
+        'business_hours','average_rating','total_reviews','is_verified','verification_documents'
+    ];
+
+    protected $casts = [
+        'business_type' => 'enum',
+        'business_hours' => 'json',
+        'average_rating' => 'decimal',
+        'total_reviews' => 'integer',
+        'is_verified' => 'boolean',
+        'verification_documents' => 'json'
+    ];
+
+
+    
 }
